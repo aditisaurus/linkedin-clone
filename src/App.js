39ea9863b912db5d720badcfac8d7f59373d1db3
aditisinghdeveloper <aditisinghdeveloper@gmail.com>
2022-05-06 21:55:34 +0530

    Setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import './App.css';
+import Header from './Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Lets build LinkedIn clone</h1>
+
+      {/* Header */}
+       <Header />
+
+      {/* App Body */}
+
+      {/*Sidebar */}
+
+      {/* Feed */}
+
+      {/* Widgets */}
 
     </div>
   );
---
+++ src/Header.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import './Header.css'
+
+function Header() {
+  return (
+    <div className="header">
+     <h1>This is Header</h1>
+
+    <div className="header_left">
+        <img src="" alt="" />
+
+        <div className="header_search">
+            {/*SearchIcon */}
+            <input type="text" />
+        </div>
+
+    </div>
+
+     
+    </div>
+  )
+}
+
+export default Header
