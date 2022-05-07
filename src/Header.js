f1c60db723f3db7aa0e5a9027d9c436fde6781bc
aditisinghdeveloper <aditisinghdeveloper@gmail.com>
2022-05-07 23:52:58 +0530

    Header

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,27 @@
 import React from 'react'
 import './Header.css'
+import SearchIcon from '@mui/icons-material/Search';
+import './HeaderOption.js';
+
 
 function Header() {
   return (
     <div className="header">
-     <h1>This is Header</h1>
 
     <div className="header_left">
-        <img src="" alt="" />
+        <img src="https://raw.githubusercontent.com/aditisinghdeveloper/linkedin-clone/master/src/linkedin.png" alt="" />
+
 
         <div className="header_search">
-            {/*SearchIcon */}
+            <SearchIcon />
             <input type="text" />
         </div>
+
+    </div>
+
+    <div className="header_right">
+        <HeaderOption title="Home"/>
+        <HeaderOption title="My Network" />
 
     </div>
 
