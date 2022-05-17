22baf5a09c043988cf74319276f0b769821f4081
aditisinghdeveloper <aditisinghdeveloper@gmail.com>
2022-05-17 23:34:30 +0530

    Redux integration

--- src/Feed.js
+++ src/Feed.js
@@ -17,7 +17,9 @@ function Feed() {
     const [posts, setPosts] = useState([]);
 
     useEffect(() => {
-        db.collection("posts").onSnapshot(snapshot => (
+        db.collection("posts")
+        .orderBy("timestamp", "desc")
+        .onSnapshot(snapshot => (
             setPosts(snapshot.docs.map(doc => (
                 {
                     id: doc.id,
@@ -40,7 +42,9 @@ function Feed() {
             photoUrl: '',
             timestamp: firebase.firestore.FieldValue.serverTimestamp()
             
-        })
+        });
+
+        setInput("");
     };
 
     
